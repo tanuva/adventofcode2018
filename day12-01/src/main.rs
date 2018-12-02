6b90af25cef49a60c0f68766acf884ff46686f03
Marcel Kummer <tanuva@posteo.de>
2018-12-02 13:13:43 +0100

    Add solution for 12-01 part 2

--- day12-01/src/main.rs
+++ day12-01/src/main.rs
@@ -34,8 +34,27 @@ fn compute_freq(start: i32, changes: Vec<i32>) -> i32 {
     return freq;
 }
 
+fn find_duplicate_freq(start: i32, changes: Vec<i32>) -> i32 {
+    let mut freq = start;
+    let mut freqs: Vec<i32> = Vec::new();
+
+    loop {
+        for change in &changes {
+            freq += change;
+
+            if freqs.contains(&freq) {
+                return freq;
+            }
+
+            freqs.push(freq);
+        }
+    };
+}
+
 fn main() -> std::io::Result<()> {
     println!("Frequency: {}", compute_freq(0, parse(read("example").unwrap())));
     println!("Frequency: {}", compute_freq(0, parse(read("input1").unwrap())));
+    println!("Duplicate freqency: {}", find_duplicate_freq(0, parse(read("example").unwrap())));
+    println!("Duplicate freqency: {}", find_duplicate_freq(0, parse(read("input1").unwrap())));
     return Ok(());
 }
